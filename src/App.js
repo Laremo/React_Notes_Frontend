5ebd2b39769e7061089f33b72a4eada064b9a77a
Laremo <memoo.lara12306@hotmail.com>
2023-07-10 22:18:01 -0600

    Login, sesion and logout implemented

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import './App.css';
 import Note from './components/Note.js';
 import { useEffect, useState } from 'react';
 import NotesService from './services/notes/NotesService.js';
+import { login } from './services/login/login';
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState('');
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(notes);
   const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const handleChange = (e) => {
-    //En cada cambio seteamos la variable newNote
-    setNewNote(e.target.value);
-    //setNewNOte retorna el valor del target
-  };
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     setLoading(true);
     NotesService.getAllNotes().then((notesJSON) => {
       if (notesJSON?.error) {
-        console.error(notesJSON.error);
+        console.log(notesJSON);
+        console.error(notesJSON.error, new Date().toISOString());
         return;
       }
       const { notes } = notesJSON;
@@ -30,24 +29,57 @@ function App() {
     });
   }, []);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const loggedUser = localStorage.getItem('loggedUser');
+    if (loggedUser) {
+      const userJson = JSON.parse(loggedUser);
+      setUser(userJson);
+      NotesService.setToken(userJson.token);
+    }
+  }, []);
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log('Submit!');
+    try {
+      const result = await login({ username, password });
+      setUser(result);
+      setUsername('');
+      setPassword('');
+      NotesService.setToken(result.token);
+      localStorage.setItem('loggedUser', JSON.stringify(result));
+    } catch (error) {
+      setErrorMessage(error.response.data);
+      setTimeout(() => setErrorMessage(''), 5000);
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    NotesService.setToken('');
+    localStorage.removeItem('loggedUser');
+  };
+
+  const handleChange = (e) => {
+    //En cada cambio seteamos la variable newNote
+    setNewNote(e.target.value);
+    //setNewNOte retorna el valor del target
   };
 
   const handleShowAll = () => {
     setShowAll(!showAll);
   };
 
-  const handleClick = (e) => {
+  const handleCrateNote = (e) => {
     //pero solo accederemos a ella cuando demos cilck
     e.preventDefault();
     const noteToAdd = {
       content: newNote,
     };
-    NotesService.createNewNote(noteToAdd).then((result) => {
-      setNotes([...notes, result]);
-      console.log(result);
+    NotesService.createNewNote(noteToAdd, {
+      token: user.token,
+      user: user._id,
+    }).then((result) => {
+      setNotes([...notes, result.response]);
     });
     setNewNote('');
   };
@@ -67,37 +99,47 @@ function App() {
     });
   };
 
+  const renderLogin = () => (
+    <form onSubmit={handleLogin}>
+      <input
+        type="text"
+        value={username}
+        placeholder="Username"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <input type="submit" value={'login'} />
+    </form>
+  );
+
+  const renderCreateNote = () => (
+    <>
+      <form onSubmit={handleCrateNote}>
+        <input type="text" onChange={handleChange} value={newNote}></input>
+        <button>Crear Nota</button>
+      </form>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </>
+  );
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
-      <div>
-        {/* Formulario temporal para login */}
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          <input type="submit" value={'login'} />
-        </form>
-        <form onSubmit={handleClick}>
-          <input type="text" onChange={handleChange} value={newNote}></input>
-          <button>Crear Nota</button>
-        </form>
-      </div>
+      {errorMessage !== '' ? <h3>{errorMessage}</h3> : null}
+      <div>{user ? renderCreateNote() : renderLogin()}</div>
       <div>
         <button onClick={handleShowAll}>
           {showAll ? 'Show Only Important' : 'Show All'}
--- src/services/notes/NotesService.js
+++ src/services/notes/NotesService.js
@@ -1,28 +1,40 @@
 const NotesService = {};
 const baseUrl = 'http://localhost:3001';
 
+let token = null;
+
+NotesService.setToken = (newToken) => {
+  token = newToken;
+};
+
 NotesService.getAllNotes = () => {
-  return fetch(baseUrl + '/api/notes')
+  return fetch(baseUrl + '/api/notes', {
+    headers: { authorization: `bearer ${token}` },
+  })
     .then((res) => res.json())
     .then((json) => {
       return json;
     });
 };
 
-NotesService.createNewNote = ({ content }) => {
+NotesService.createNewNote = ({ content }, { user }) => {
   return fetch(baseUrl + '/api/notes', {
     method: 'POST',
     body: JSON.stringify({
       note: {
         content: content,
+        user: user,
+        important: Math.random() > 0.5,
       },
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
+      authorization: `bearer ${token}`,
     },
   })
     .then((response) => response.json())
     .then((json) => {
+      console.log(json);
       return json;
     });
 };
